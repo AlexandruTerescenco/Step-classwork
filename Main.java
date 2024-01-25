6ed8565c71e5e0d12a8d3df490a65361c07b3435
Alexandru Terescenco <alexandruterescencp@gmail.com>
2024-01-25 18:58:46 +0200

    par sau impar

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		
+		int x = 21;
+		if (x%2 == 0) {
+			System.out.println("x e par");
+		} else {
+			System.out.println("x e impar");
+		}
 	}
 }
